0a7a16eefd03b785889df40feda3b1c3be21ded8
olgatar <leyendaorama@gmail.com>
2015-09-29 10:44:02 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-$('article p, li.mylist').hide();
+$('article p, ul.mylist').hide();
 
 $('h4').click(function(){
-  $(this).next('li.mylist').slideToggle();
+  $(this).next('ul.mylist').slideToggle();
 })
