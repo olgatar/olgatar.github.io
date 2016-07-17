b0d24c2dd3b8bef03342c83768da7ef31e7324a5
olgatar <leyendaorama@gmail.com>
2016-07-17 19:34:52 +0200

    complete redesign of my DPY

--- js/main.js
+++ js/main.js
@@ -1,5 +1,34 @@
-$('article p').hide();
-
-$('h4').click(function(){
-  $(this).next().slideToggle();
-})
+$(document).ready(function() {
+	// navigation click actions
+	$('.scroll-link').on('click', function(event){
+		event.preventDefault();
+		var sectionID = $(this).attr("data-id");
+		scrollToID('#' + sectionID, 750);
+	});
+	// scroll to top action
+	$('.scroll-top').on('click', function(event) {
+		event.preventDefault();
+		$('html, body').animate({scrollTop:0}, 'slow');
+	});
+	// mobile nav toggle
+	$('#nav-toggle').on('click', function (event) {
+		event.preventDefault();
+		$('#myNavbar').toggleClass("open");
+	});
+});
+// scroll function
+function scrollToID(id, speed){
+	var offSet = 80;
+	var targetOffset = $(id).offset().top - offSet;
+	var mainNav = $('#myNavbar');
+	$('html,body').animate({scrollTop:targetOffset}, speed);
+	if (mainNav.hasClass("open")) {
+		mainNav.css("height", "1px").removeClass("in").addClass("collapse");
+		mainNav.removeClass("open");
+	}
+}
+if (typeof console === "undefined") {
+    console = {
+        log: function() { }
+    };
+}
